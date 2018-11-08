b798b95072470aa307d7fb24eb07301ab046ff3b
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro-6.local>
2018-11-08 15:32:42 +0300

    Intro to GraphQL

---
+++ index.js
@@ -0,0 +1,39 @@
+const { graphql, buildSchema } = require('graphql');
+
+const db = {
+  users: [
+    { id: '1', email: 'alex@gmail.com', name: 'Alex' },
+    { id: '2', email: 'max@gmail.com', name: 'Max' }
+  ]
+};
+const schema = buildSchema(`
+    type Query {
+        users: [User!]!
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        name: String
+        avatarUrl: String
+    }
+`);
+
+const rootValue = {
+  users: () => db.users
+};
+
+graphql(
+  schema,
+  `
+    {
+      users {
+        email
+        name
+      }
+    }
+  `,
+  rootValue
+)
+  .then(res => console.dir(res, { depth: null }))
+  .catch(console.error);
